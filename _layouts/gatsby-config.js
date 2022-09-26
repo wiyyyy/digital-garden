a44a8fb25172fef2dd2a57a67b9b28b8775e4d0c
ahmet.ak <ahmet.ak@foriba.com>
2022-09-26 22:25:13 +0300

    .

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -4,11 +4,11 @@ const PATH_PREFIX = process.env.PATH_PREFIX
 
 module.exports = {
   // pathPrefix: PATH_PREFIX || `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
-  pathPrefix: PATH_PREFIX || `/`, // b. If you are using Netlify/Vercel, your can keep it this way
+  pathPrefix: PATH_PREFIX || `/digital-garden`, // b. If you are using Netlify/Vercel, your can keep it this way
   siteMetadata: {
     // some SEO configs using by gatsby-theme-kb
-    title: `Foam`, // Replace it with your site's title
-    author: `Your Name`, // Replace it with your name
+    title: `My Digital Garden`, // Replace it with your site's title
+    author: `Ahmet AK`, // Replace it with your name
     description: `My personal knowledge base`, // Replace it with your site's description
   },
   plugins: [
